4e1b6eccaed154a82f0e722bc887e6d0302a1fa3
eberty <eberty.silva@hotmail.com>
2017-11-03 21:33:39 -0300

    Quase tudo Pronto. Edicoes EbertyFelipe NBCGIB 03/11

--- Codigo/tokens.h
+++ Codigo/tokens.h
@@ -1,65 +1,6 @@
 #ifndef _TOKENS_H_   // Nao incluir duas vezes
 #define _TOKENS_H_
 
-//Includes
-#include "strings.h"
 
-//Macros
-#define QUANTIDADE_DE_TOKENS 41
-#define TAMANHO_DO_MAIOR_TOKEN 15
-
-int resumo[QUANTIDADE_DE_TOKENS] = {0};
-
-
-//typedefs
-typedef enum {
-	tk_EOF,
-	tk_IDEN,
-	tk_INTEIRO,
-	tk_DECIMAL,
-	tk_CADEIA,
-	tk_inicio,
-	tk_fim,
-	tk_int,
-	tk_dec,
-	tk_leia,
-	tk_imprima,
-	tk_para,
-	tk_de,
-	tk_ate,
-	tk_passo,
-	tk_fim_para,
-	tk_se,
-	tk_entao,
-	tk_senao,
-	tk_fim_se,
-	tk_e,
-	tk_ou,
-	tk_nao,
-	tk_virg,
-	tk_pt_virg,
-	tk_dois_pts,
-	tk_abre_par,
-	tk_fecha_par,
-	tk_menor,
-	tk_menor_igual,
-	tk_maior,
-	tk_maior_igual,
-	tk_diferente,
-	tk_igual,
-	tk_incr,
-	tk_decr,
-	tk_atrib,
-	tk_mais,
-	tk_menos,
-	tk_vezes,
-	tk_dividido
-} tToken;
-
-
-
-//Prototipos
-
-//Funcoes
 
 #endif
